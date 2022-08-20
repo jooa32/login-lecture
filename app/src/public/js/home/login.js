8e6cbe296504e5d89ac2eea1ae9acad670e9d410
jooa <dlwndk4677@gmail.com>
2022-08-20 15:08:55 +0900

    commit

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,13 @@
 "use strict";
 
-console.log("hello");
+// console.log("hello");
+const id = document.querySelector("#id");
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
+
+
+loginBtn.addEventListener("click", login);
+function login(){
+    console.log(id.value);
+    console.log(psword.value);
+}
--- bin/www.js
+++
@@ -1,8 +0,0 @@
-"use stric";
-
-const app = require("../app");
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log("서버가동");
-});
