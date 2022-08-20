76b8af10b53c8ce0b69e51f0e4b3eccf9dfd7fe8
jooa <dlwndk4677@gmail.com>
2022-08-20 12:36:12 +0900

    second commit

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
+"use strict";
 const express = require("express");
 const app = express();
-
+const PORT = 3000;
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+const home = require("./routes/home");
+app.use("/", home);
 
-app.get("/", (req, res) => {
-    res.render("home/index");
-});
-
-app.get("/login", (req, res) => {
-    res.render("home/login")
-});
-
-app.listen(3000, function(){
+app.listen(PORT, function(){
     console.log("서버가동");
-});
+}); 
